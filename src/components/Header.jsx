b75e746eb2752338d3c0ef3e2bb7e43db19fc7e4
Akash NP <akashnp1998@gmail.com>
2022-11-14 22:24:48 +0530

    Improve Header by adding interactivity
    - Inspired by Lee Robin's personal website header menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,46 @@
+import { useState } from "react"
+
 export default function Header() {
+    const [homeActive, setHomeActive ] = useState(true);
+    const [blogActive, setBlogActive ] = useState(false);
+    const [contactActive, setContactActive ] = useState(false);
+
+    function handleClick(event) {
+        const eventSource = event.target.innerHTML;
+        if ( eventSource == 'Home' )  {
+            setHomeActive(true);
+            setBlogActive(false);
+            setContactActive(false);
+        } else if ( eventSource == 'Blog' ) {
+            setHomeActive(false);
+            setBlogActive(true);
+            setContactActive(false);
+        } else {
+            setHomeActive(false);
+            setBlogActive(false);
+            setContactActive(true);
+        }
+    }
+
     return (
         <header className="py-8">
-            <nav className="flex gap-10">
-                <div className="text-neutral-400 text-lg">
+            <nav className="flex items-center gap-3 md:gap-8">
+                <div 
+                    className={`${homeActive ? 'font-bold text-white' : ''} text-neutral-400 text-lg p-2 rounded-lg hover:bg-neutral-800 cursor-pointer`}
+                    onClick={handleClick}
+                >
                     Home
                 </div>
-                <div className="text-neutral-400 text-lg">
+                <div 
+                    className={`${blogActive ? 'font-bold text-white' : ''} text-neutral-400 text-lg p-2 rounded-lg hover:bg-neutral-800 cursor-pointer`}
+                    onClick={handleClick}
+                >
                     Blog
                 </div>
-                <div className="text-neutral-400 text-lg">
+                <div 
+                    className={`${contactActive ? 'font-bold text-white' : ''} text-neutral-400 text-lg p-2 rounded-lg hover:bg-neutral-800 cursor-pointer`}
+                    onClick={handleClick}
+                >
                     Contact
                 </div>
             </nav>
