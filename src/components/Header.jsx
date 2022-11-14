ca3b52a9b066841dc84f6874b8739c6272e4aecf
Akash NP <akashnp1998@gmail.com>
2022-11-14 21:25:17 +0530

    Add Header Component, Embed in Layout component

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+export default function Header() {
+    return (
+        <header className="py-8">
+            <nav className="flex gap-10">
+                <div className="text-neutral-400 text-lg">
+                    Home
+                </div>
+                <div className="text-neutral-400 text-lg">
+                    Blog
+                </div>
+                <div className="text-neutral-400 text-lg">
+                    Contact
+                </div>
+            </nav>
+        </header>
+    )
+}
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,7 +1,9 @@
+import Header from "./Header"
 export default function Layout({children}) {
     return (
         <main className="flex px-8 bg-white dark:bg-neutral-900 text-black dark:text-white">
             <div className="mx-auto max-w-5xl">
+                <Header/>
                 {children}
             </div>
         </main>
