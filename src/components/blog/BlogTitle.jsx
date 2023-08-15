386bd3e699305fa3aaeaf787b90dc18107c3a1f4
Akash N P <akashnp1998@gmail.com>
2023-08-15 22:38:17 +0400

    feat: add basic scroll-driven animation to BlogTitle
    adds a bit more finesse to the page
    
    NOTE: experimental feature as of commit date

--- src/components/blog/BlogTitle.jsx
+++ src/components/blog/BlogTitle.jsx
@@ -1,4 +1,4 @@
 
 export default function BlogTitle({ title }) {
-    return <h1 className="text-3xl font-bold pb-3" >{title}</h1>
+    return <h1 className="font-bold pb-3 selector">{title}</h1>
 }
