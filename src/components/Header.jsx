e10cd35d57fde71afc99f6558d25d2bcdecf8574
Akash NP <akashnp1998@gmail.com>
2023-10-29 10:47:05 +0400

    style: minor improvements

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,30 +5,23 @@ export default function Header() {
         <header className="py-8">
             <nav className="flex items-center gap-3 md:gap-8">
                 <a 
-                    className={`text-neutral-400 text-lg p-2 rounded-lg hover:bg-neutral-800 cursor-pointer`}
+                    className={`text-neutral-400 text-md p-2 rounded-lg hover:bg-neutral-800 cursor-pointer`}
                     href="/"
                 >
-                    Home
+                    home
                 </a>
                 <a 
-                    className={`text-neutral-400 text-lg p-2 rounded-lg hover:bg-neutral-800 cursor-pointer`}
+                    className={`text-neutral-400 text-md p-2 rounded-lg hover:bg-neutral-800 cursor-pointer`}
                     href="/blog"
                 >
-                    Blog
+                    blog
                 </a>
                 <a 
-                    className={`text-neutral-400 text-lg p-2 rounded-lg hover:bg-neutral-800 cursor-pointer`}
+                    className={`text-neutral-400 text-md p-2 rounded-lg hover:bg-neutral-800 cursor-pointer`}
                     href="/notes"
                 >
-                    Notes
+                    notes
                 </a>
-                <abbr title="work in progress :)" className="contact-abbr">
-                    <a 
-                        className={`text-neutral-400 text-lg p-2 rounded-lg hover:bg-neutral-800 cursor-pointer`}
-                    >
-                        Contact
-                    </a>
-                </abbr>
             </nav>
         </header>
     )
