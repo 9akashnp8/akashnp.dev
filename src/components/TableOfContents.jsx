9b369954d49fe179952d145fa90f09de34949625
Akash NP <akashnp1998@gmail.com>
2024-01-26 22:28:20 +0400

    chore: fix toc text color

--- src/components/TableOfContents.jsx
+++ src/components/TableOfContents.jsx
@@ -7,13 +7,13 @@ export default function TableOfContents({ headings }) {
   return (
     <aside id="table-of-contents" className="hidden lg:block sticky top-10 self-start">
       <h2 className="font-bold text-xl pb-3">Contents</h2>
-      <ul className="text-slate-400">
+      <ul className="text-stone-400">
         {headings.map((heading) => {
           if (heading.depth === 2) {
             return (
               <li
                 key={heading.slug}
-                className="pb-3 hover:text-slate-100"
+                className="pb-3 hover:text-stone-100"
               >
                 <Heading heading={heading} />
               </li>
@@ -21,7 +21,7 @@ export default function TableOfContents({ headings }) {
           } else if (heading.depth === 3) {
             return (
               <ul key={heading.slug} className="ml-5">
-                <li className="pb-3 hover:text-slate-100">
+                <li className="pb-3 hover:text-stone-100">
                   <Heading heading={heading} />
                 </li>
               </ul>
